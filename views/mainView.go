ad7610dd289da995529e08562734c2cbfb7c0bff
Anorak01 <adamfri007@gmail.com>
2025-06-23 00:00:16 +0200

    Basically implemented all the functionality

--- views/mainView.go
+++ views/mainView.go
@@ -6,43 +6,54 @@ import (
 	"fmt"
 	tea "github.com/charmbracelet/bubbletea"
 	"os"
-	"strconv"
+	"os/exec"
 )
 
 type sshEntry struct {
-	Name        string
-	Host        string
-	Port        string
-	User        string
-	Password    string // password, if not set a key is used
-	Key         string // path to private key file
-	KeyPassword string // password to unlock the key, if key is set
+	Name string
+	Host string
+	Port string
+	User string
+	Key  string // path to private key file
 }
 
 type data struct {
-	TerminalName string     // terminal used to open
+	TerminalName string     // terminal used to open ssh connection
 	SshEntries   []sshEntry // list of SSH entries
+	HideData     bool       // hide sensitive data
+	dataDir      string
 }
 
 type model struct {
-	cursor        int  // which to-do list item our cursor is pointing at
-	data          data // data to be saved in the file
+	cursor        int  // which ssh entry cursor is pointing at
+	data          data // data to be saved in a file
 	newConnection sshEntry
+	termHeight    int
 }
 
-func InitialModel() model {
-	return model{
-		data: data{"", []sshEntry{}},
+func InitialModel() *model {
+	return &model{
+		data: data{"kitty", []sshEntry{}, false, ""},
 	}
 }
 
-func (m model) Init() tea.Cmd {
-	// Just return `nil`, which means "no I/O right now, please."
+func (m *model) Init() tea.Cmd {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Printf("Error getting home directory: %v\n", err)
+		os.Exit(1)
+	}
+	m.data.dataDir = home + "/.sshtui/ssh_tui.json"
+	print("Data directory: " + m.data.dataDir + "\n")
 
-	// Check if the file exists, if not create it
-	if _, err := os.Stat("./ssh_tui.json"); errors.Is(err, os.ErrNotExist) {
+	// check if the file exists, if not create it
+	if _, err := os.Stat(m.data.dataDir); errors.Is(err, os.ErrNotExist) {
 		fmt.Println("File ssh_tui.json does not exist, creating a new one.")
-		file, err := os.Create("./ssh_tui.json")
+		err := os.Mkdir(home+"/.sshtui", os.ModePerm) // make the directory just in case
+		if err != nil {
+			return nil
+		}
+		file, err := os.Create(m.data.dataDir)
 		if err != nil {
 			fmt.Printf("Error creating file: %v\n", err)
 			return nil
@@ -60,117 +71,122 @@ func (m model) Init() tea.Cmd {
 			return nil
 		}
 	}
-	dat, err := os.ReadFile("./ssh_tui.json") //UserHomeDir()
+
+	// read the file and unmarshal the data
+	dat, err := os.ReadFile(m.data.dataDir)
 	if err != nil {
 		fmt.Printf("Error reading file: %v\n", err)
 		return nil
 	}
-	fmt.Print(string(dat))
+	err = json.Unmarshal(dat, &m.data)
+	if err != nil {
+		fmt.Printf("Error unmarshalling data: %v\n", err)
+		return nil
+	}
+
 	return nil
 }
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (m *model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.termHeight = msg.Height
 
-	// Is it a key press?
 	case tea.KeyMsg:
-
-		// Cool, what was the actual key pressed?
 		switch msg.String() {
-
-		// These keys should exit the program.
+		// these should exit the program.
 		case "ctrl+c", "q":
 			return m, tea.Quit
 
-		// The "up" and "k" keys move the cursor up
 		case "up", "k":
 			if m.cursor > 0 {
 				m.cursor--
 			}
 
-		// The "down" and "j" keys move the cursor down
 		case "down", "j":
 			if m.cursor < len(m.data.SshEntries)-1 {
 				m.cursor++
 			}
 
-		// The "enter" key and the spacebar (a literal space) toggle
-		// the selected state for the item that the cursor is pointing at.
+		case "home":
+			// hide sensitive data
+			m.data.HideData = !m.data.HideData
+			// save the updated data to the file
+			saveData(m.data)
+
 		case "enter", " ":
 			// open the selected SSH entry
-			return newConnectionView(m).Update(msg)
-		case "n":
-			// Add a new SSH entry
-			name := "New SSH Entry " + strconv.Itoa(len(m.data.SshEntries)+1)
-			newEntry := sshEntry{
-				Name:        name,
-				Host:        "new_host",
-				Port:        "22",
-				User:        "new_user",
-				Password:    "",
-				Key:         "",
-				KeyPassword: "",
+			selectedEntry := m.data.SshEntries[m.cursor]
+			sshArgs := []string{"ssh"}
+			sshArgs = append(sshArgs, fmt.Sprintf("%s@%s", selectedEntry.User, selectedEntry.Host))
+			if selectedEntry.Port != "" {
+				sshArgs = append(sshArgs, "-p", selectedEntry.Port)
 			}
-			m.data.SshEntries = append(m.data.SshEntries, newEntry)
+			if selectedEntry.Key != "" { // has key
+				sshArgs = append(sshArgs, "-i", selectedEntry.Key)
+			}
+
+			err := exec.Command(m.data.TerminalName, sshArgs...).Run()
+			if err != nil {
+				// print error
+				fmt.Printf("Error running command: %v\n", err)
+
+			}
+
+		case "n": // open new connection view
+			return newConnectionView(m, m.termHeight).Update(msg)
 
 		case "d":
-			// Delete the selected SSH entry
+			// delete the selected SSH entry
 			if len(m.data.SshEntries) > 0 && m.cursor < len(m.data.SshEntries) {
-				// Remove the entry at the cursor position
+				// remove the entry at the cursor position
 				m.data.SshEntries = append(m.data.SshEntries[:m.cursor], m.data.SshEntries[m.cursor+1:]...)
-				// Adjust the cursor if necessary
+				// adjust the cursor if necessary
 				if m.cursor >= len(m.data.SshEntries) {
 					m.cursor = len(m.data.SshEntries) - 1
 				}
+				// save the updated data to the file
+				saveData(m.data)
 			}
 		}
 	}
-
-	// Return the updated model to the Bubble Tea runtime for processing.
-	// Note that we're not returning a command.
 	return m, nil
 }
 
-func (m model) View() string {
-	// The header
-	s := "SSH connections:\n\n"
-
-	// Iterate over our choices
-	/*for i, choice := range m.choices {
-
-		// Is the cursor pointing at this choice?
-		cursor := " " // no cursor
-		if m.cursor == i {
-			cursor = ">" // cursor!
-		}
-
-		// Is this choice selected?
-		checked := " " // not selected
-		if _, ok := m.selected[i]; ok {
-			checked = "x" // selected!
-		}
-
-		// Render the row
-		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
-	}*/
-
-	for i, entry := range m.data.SshEntries {
-
-		// Is the cursor pointing at this choice?
-		cursor := " " // no cursor
-		if m.cursor == i {
-			cursor = ">" // cursor!
-		}
-
-		// Is this choice selected?
-		checked := " " // not selected
-
-		// Render the row
-		s += fmt.Sprintf("%s [%s] %s %s@%s:%s\n", cursor, checked, entry.Name, entry.User, entry.Host, entry.Port)
+func saveData(m data) {
+	// save the current data to the file
+	dat, err := json.MarshalIndent(m, "", "  ")
+	if err != nil {
+		fmt.Printf("Error marshalling data: %v\n", err)
+		return
 	}
 
-	// The footer
+	err = os.WriteFile(m.dataDir, dat, 0644)
+	if err != nil {
+		fmt.Printf("Error writing to file: %v\n", err)
+		return
+	}
+}
+
+func (m *model) View() string {
+	s := "SSH connections: " + "\n\n"
+
+	for i, entry := range m.data.SshEntries {
+		cursor := " " // no cursor
+		if m.cursor == i {
+			cursor = ">" // cursor!
+		}
+
+		if !m.data.HideData {
+			// render the row
+			s += fmt.Sprintf("%s [] %s %s@%s:%s\n", cursor, entry.Name, entry.User, entry.Host, entry.Port)
+		} else {
+			// render the row with hidden data
+			s += fmt.Sprintf("%s [] %s %s@%s\n", cursor, entry.Name, "***", "****")
+		}
+
+	}
+
 	s += "\n| Q quit | N new | D delete |\n"
 
-	// Send the UI for rendering
 	return s
 }
--- views/newConnectionView.go
+++ views/newConnectionView.go
@@ -1,55 +1,209 @@
 package views
 
 import (
+	"anorak01.top/sshtui/views/filepicker"
+	"errors"
 	tea "github.com/charmbracelet/bubbletea"
+	"os"
+	"strconv"
+	"time"
 	"unicode"
 	"unicode/utf8"
 )
 
-type newConnectionViewModel struct {
-	String      string
-	returnModel model
+type state int
+
+const (
+	StateName state = iota
+	StateHost
+	StatePort
+	StateUser
+	StateIfKey
+	StateKey
+	StateDone
+)
+
+var stateNames = map[state]string{
+	StateName:  "Name",
+	StateHost:  "Host",
+	StatePort:  "Port",
+	StateUser:  "User",
+	StateIfKey: "Key? (y/N)",
+	StateKey:   "Private Key",
+	StateDone:  "Done",
 }
 
-func newConnectionView(returnTo model) newConnectionViewModel {
-	return newConnectionViewModel{returnModel: returnTo}
+type newConnectionViewModel struct {
+	String       string
+	State        state
+	SshEntry     sshEntry
+	filePicker   filepicker.Model
+	selectedFile string
+	err          error
+	returnModel  *model
+}
+
+type clearErrorMsg struct{}
+
+func clearErrorAfter(t time.Duration) tea.Cmd {
+	return tea.Tick(t, func(_ time.Time) tea.Msg {
+		return clearErrorMsg{}
+	})
+}
+
+func newConnectionView(returnTo *model, initialHeight int) newConnectionViewModel {
+	fp := filepicker.New()
+	fp.CurrentDirectory, _ = os.UserHomeDir()
+	fp.AutoHeight = true
+	fp.SetHeight(initialHeight - 1)
+	fp.ShowHidden = true
+
+	return newConnectionViewModel{returnModel: returnTo, filePicker: fp}
 }
 
 func (m newConnectionViewModel) Init() tea.Cmd {
-	// Initialize the new connection view
-	// This could include setting up any necessary state or UI elements
-	return nil
+	// initialize the new connection view model and file picker
+	m.String = ""
+	m.State = StateName
+	m.SshEntry = sshEntry{}
+	return m.filePicker.Init()
 }
 
 func (m newConnectionViewModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.filePicker.SetHeight(msg.Height - 1)
+		m.filePicker.Update(msg)
+
 	case tea.KeyMsg:
 		switch msg.Type {
 		case tea.KeyCtrlC:
-			// Handle Ctrl+C to exit the application
+			// handle Ctrl+C to exit the application
 			return m, tea.Quit
 		case tea.KeyEsc:
-			// Handle the Esc to go back to the main view
+			// handle the Esc to go back to the main view
 			return m.returnModel.Update(msg)
 		case tea.KeyEnter:
-			// Handle the Enter key to save the connection name
+			// process the input based on the current state
+			switch m.State {
+			case StateName:
+				if m.String == "" {
+					return m, nil
+				}
+				m.SshEntry.Name = m.String
+				m.String = ""
+				m.State = StateHost
+			case StateHost:
+				if m.String == "" {
+					return m, nil
+				}
+				// no host validation done, you better know what you're doing
+				m.SshEntry.Host = m.String
+				m.String = ""
+				m.State = StatePort
+			case StatePort:
+				if m.String == "" {
+					return m, nil
+				}
+				// do port validation
+				if port, err := strconv.Atoi(m.String); err != nil && port < 1 || port > 65535 {
+					return m, nil
+				}
+				m.SshEntry.Port = m.String
+				m.String = ""
+				m.State = StateUser
+			case StateUser:
+				if m.String == "" {
+					return m, nil
+				}
+				m.SshEntry.User = m.String
+				m.String = ""
+				m.State = StateIfKey
+			case StateIfKey:
+				if m.String == "y" || m.String == "Y" {
+					// if user wants key, prompt for key
+					m.State = StateKey
+					m.String = ""
+				} else {
+					// If the user does not want to use a key, move to the done state
+					m.SshEntry.Key = ""
+					m.State = StateDone
+				}
+
+			case StateKey:
+				// get the private key, probably use a file picker
+				m.filePicker.Update(tea.KeyEnter)
+			case StateDone:
+				// save everything and return to the main view
+				m.returnModel.data.SshEntries = append(m.returnModel.data.SshEntries, m.SshEntry)
+				saveData(m.returnModel.data)
+				// return to main view
+				return m.returnModel.Update(nil)
+			}
+
 		case tea.KeyBackspace:
-			// Handle the Backspace key to remove the last character
+			// remove last character
 			if len(m.String) > 0 {
 				m.String = m.String[:len(m.String)-1]
 			}
 		default:
-			if utf8.RuneCountInString(msg.String()) == 1 && unicode.IsPrint([]rune(msg.String())[0]) {
-				m.String += msg.String()
+			if m.State != StateKey {
+				if utf8.RuneCountInString(msg.String()) == 1 && unicode.IsPrint([]rune(msg.String())[0]) {
+					m.String += msg.String()
+				}
 			}
 		}
 	}
+	if m.State == StateKey {
+		var cmd tea.Cmd
+		m.filePicker, cmd = m.filePicker.Update(msg)
+
+		// check if the user selected a file
+		if didSelect, path := m.filePicker.DidSelectFile(msg); didSelect {
+			// get path of the selected file
+			m.selectedFile = path
+			m.SshEntry.Key = path
+			m.State = StateDone
+			return m, nil
+		}
+
+		// check if user selected invalid file
+		if didSelect, path := m.filePicker.DidSelectDisabledFile(msg); didSelect {
+			// clear selected file and show error
+			m.err = errors.New(path + " is not valid.")
+			m.selectedFile = ""
+			return m, tea.Batch(cmd, clearErrorAfter(2*time.Second))
+		}
+
+		return m, cmd
+	}
+
 	return m, nil
 }
 
 func (m newConnectionViewModel) View() string {
-	// Render the new connection view
-	s := "Enter Connection Name: "
-	s += m.String + "\n"
+	// render the new connection view
+	s := ""
+	if m.State == StateKey {
+		s += m.filePicker.View()
+	} else if m.State == StateDone {
+		// print out the summary of the SSH entry
+		s += "SSH Connection Summary:\n\n"
+		s += "Name: " + m.SshEntry.Name + "\n"
+		s += "Host: " + m.SshEntry.Host + "\n"
+		if m.SshEntry.Port != "" {
+			s += "Port: " + m.SshEntry.Port + "\n"
+		}
+		s += "User: " + m.SshEntry.User + "\n"
+		if m.SshEntry.Key != "" {
+			s += "Auth: " + m.SshEntry.Key + "\n"
+		} else {
+			s += "Auth: Password\n"
+		}
+		s += "\n| Enter save | Esc cancel |\n\n"
+	} else {
+		s = "Enter " + stateNames[m.State] + ":\n\n"
+		s += m.String + "\n"
+	}
 	return s
 }
